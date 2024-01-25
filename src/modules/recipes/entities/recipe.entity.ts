8a7e4237da35d71fde95851d78e938498fa798ea
Suldery Arellano <sulderyarellano18@gmail.com>
2024-01-25 18:58:21 -0500

    Recipe entity

--- src/modules/recipes/entities/recipe.entity.ts
+++ src/modules/recipes/entities/recipe.entity.ts
@@ -1,1 +1,61 @@
-export class Recipe {}
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  BeforeInsert,
+  BeforeUpdate,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity('recipes')
+export class Recipe {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  coverImage: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  steps: string;
+
+  @Column({
+    type: 'int',
+    nullable: false,
+  })
+  portions: number;
+
+  @Exclude()
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Exclude()
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
