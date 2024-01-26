e9d33047428bb1c2961c4afb61cdd74777c37d46
Suldery Arellano <sulderyarellano18@gmail.com>
2024-01-25 19:03:11 -0500

    Subject entity

--- src/modules/subjects/entities/subject.entity.ts
+++ src/modules/subjects/entities/subject.entity.ts
@@ -1,1 +1,44 @@
-export class Subject {}
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  BeforeInsert,
+  BeforeUpdate,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity('subjects')
+export class Subjects {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    unique: true,
+  })
+  name: string;
+
+  @Exclude()
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Exclude()
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
