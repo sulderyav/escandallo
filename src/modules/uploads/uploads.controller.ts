ddc2a3c5b6885c3030e677c5014ea213dd6bf302
Suldery Arellano <sulderyarellano18@gmail.com>
2024-04-04 09:45:52 -0500

    Upload ingredient images

--- src/modules/uploads/uploads.controller.ts
+++ src/modules/uploads/uploads.controller.ts
@@ -95,4 +95,68 @@ export class UploadsController {
 
     return uploadedFile;
   }
+
+  @Roles(RoleNames.ADMIN, RoleNames.TEACHER)
+  @Post('/ingredients/images')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: 'temp/',
+        filename: (req, file, cb) => {
+          const splittedName = file.originalname.split('.');
+          const ext = splittedName[splittedName.length - 1];
+
+          cb(null, `${uuid()}.${ext}`);
+        },
+      }),
+    }),
+  )
+  async uploadIngredientImages(
+    @Req() req,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    if (!file)
+      throw new HttpExceptionMessage(
+        HttpStatus.BAD_REQUEST,
+        'File is required',
+      );
+
+    const fileExtension = file.originalname.split('.').pop();
+    const fileSizes = file.size / 1024; // in kb
+    const localFilePath = file.path;
+
+    if (
+      fileExtension !== 'png' &&
+      fileExtension !== 'jpg' &&
+      fileExtension !== 'jpeg'
+    ) {
+      this.uploadsService.deleteFile(localFilePath);
+      throw new HttpExceptionMessage(
+        HttpStatus.BAD_REQUEST,
+        'File must be a .png .jpg .jpeg file',
+      );
+    }
+
+    if (fileSizes > this.MAX_PRODUCT_IMAGE_SIZE_IN_KB) {
+      this.uploadsService.deleteFile(localFilePath);
+      throw new HttpExceptionMessage(
+        HttpStatus.BAD_REQUEST,
+        'File size must be less than 5MB',
+      );
+    }
+
+    const fileName = file.filename;
+    const filePath = `${INGREDIENTS_IMAGES_PATH}/${uuid()}-${fileName}`;
+    const fileStream = createReadStream(localFilePath);
+
+    const uploadedFile = await this.uploadsService.uploadFileAndMakeItPublic(
+      fileStream as any,
+      filePath,
+    );
+
+    // Delete the file after it's uploaded to the cloud
+    this.uploadsService.deleteFile(localFilePath);
+
+    return uploadedFile;
+  }
 }
